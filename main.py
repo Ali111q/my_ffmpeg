c8758853a65cdd21a9617d9f8f400dfb71f58aa4
ali <bbbeat114@gmail.com>
2023-03-02 21:31:09 +0300

    main

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from moviepy.video.io.VideoFileClip import VideoFileClip
 import moviepy.video.fx.all as vfx
 
 # Open the input video
-clip = VideoFileClip('./IMG_1635.MOV')
+clip = VideoFileClip('./hh.mp4')
 
 # Define the new size
 new_width = 854
@@ -12,4 +12,4 @@ new_height = 480
 resized_clip = vfx.resize(clip, (new_width, new_height))
 
 # Save the resized video to a new file
-resized_clip.write_videofile('gpu.mp4', bitrate="400k", codec='h264')
+resized_clip.write_videofile('gpu2.mp4', bitrate="400k", codec='h264')
