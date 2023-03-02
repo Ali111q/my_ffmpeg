9a8405f628ffde27e44128463f08f4389c154dbd
ali <bbbeat114@gmail.com>
2023-03-02 22:05:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -12,8 +12,4 @@ new_height = 480
 resized_clip = vfx.resize(clip, (new_width, new_height))
 
 # Save the resized video to a new file
-resized_clip.write_videofile('weff.mp4',
-    bitrate="400k",
-    codec='libx264',
-    preset='ultrafast',
-    ffmpeg_params=['-c:v', 'h264_nvenc', '-c:a', 'aac', '-strict', 'experimental'])
+resized_clip.write_videofile('weff.mp4', bitrate="400k", codec='libx264', preset='ultrafast', ffmpeg_params=['-hwaccel', 'auto', '-i', './hh.mp4'])
